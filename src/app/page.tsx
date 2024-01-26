d1cde0ceebafd64309decfa0d7de39513de349fd
Nachiket - Pushkarna <nachiketpushkarna@gmail.com>
2024-01-26 14:35:53 +0530

    fifth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,61 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobResults from "@/components/JobResults";
 import { JobFilterValues } from "@/lib/validation";
-
+import { Metadata } from "next";
 
 interface PageProps {
-  searchParams : {
-    q?: string,
-    type?: string,
-    location?: string,
-    remote?: string,
-
-  }
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+  };
 }
 
-export default async function Home({ searchParams: {q,type,location,remote}}: PageProps) {
+// Dynamically render the Main title  for the searched jobs
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
 
+  const titleSuffix = location ? `in ${location}` : "";
+
+  return `${titlePrefix} ${titleSuffix}`;
+}
+
+//Dynamically render the metadata for the title of the Page
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: PageProps): Metadata {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Nachii Jobs`,
+  };
+}
+
+export default async function Home({
+  searchParams: { q, type, location, remote },
+}: PageProps) {
   const filterValues: JobFilterValues = {
     q,
     type,
     location,
     remote: remote === "true",
-  }
+  };
 
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
         <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Developer Jobs for you
+          {getTitle(filterValues)}
         </h1>
         <p className="text-muted-foreground">Find your dream Job</p>
       </div>
